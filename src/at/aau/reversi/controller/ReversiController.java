600b15b39a4d855289df54c995952390e3654bb8
biking-flip <phips203@gmx.at>
2013-03-21 16:30:13 +0100

    Implement startGame method

--- src/at/aau/reversi/controller/ReversiController.java
+++ src/at/aau/reversi/controller/ReversiController.java
@@ -2,20 +2,54 @@ package at.aau.reversi.controller;
 
 import java.util.Observable;
 
+import at.aau.reversi.Constants;
 import at.aau.reversi.bean.GameBean;
+import at.aau.reversi.logic.AI;
+import at.aau.reversi.logic.GameLogic;
+import at.aau.reversi.logic.GameLogicLocalImpl;
+import at.aau.reversi.logic.GameLogicNetworkImpl;
+import at.aau.reversi.logic.WeakAIImpl;
 
 public class ReversiController extends Observable {
 
+	private GameBean gameBean;
+	private AI whiteAI;
+	private AI blackAI;	
+	private GameLogic logic;
+
 	public ReversiController(){
-		gameBean = new GameBean();
 	}
 	
 	/**
 	 * Starts a new Game
 	 * @param playerTypeWhite
 	 * @param playerTypeBlack
+	 * @param isHost When Game over Network and Acts as host
 	 */
-	public void startGame(short playerTypeWhite, short playerTypeBlack){
+	public void startGame(short playerTypeWhite, short playerTypeBlack, boolean isHost){
+		
+		// Init Game
+		gameBean = new GameBean();
+		
+		if(playerTypeWhite == Constants.PLAYER_TYPE_AI){
+			whiteAI = new WeakAIImpl();
+		}
+		if(playerTypeBlack == Constants.PLAYER_TYPE_AI){
+			blackAI = new WeakAIImpl();
+		}
+		if(!isHost){
+			logic = new GameLogicLocalImpl();
+		}else{
+			logic = new GameLogicNetworkImpl();
+		}
+		
+		// When white gamer is human, unfreeze gamefield
+		if(playerTypeWhite == Constants.PLAYER_TYPE_HUMAN_PLAYER){
+			gameBean.setGameFieldActive(true);
+		}
+		
+		setChanged();
+		notifyObservers();
 		
 	}
 	
@@ -30,7 +64,6 @@ public class ReversiController extends Observable {
 		// TODO: 
 	}
 	
-	private GameBean gameBean;
 
 	/**
 	 * @return the gameBean
