d1c815591d8b6a2325fe4a86cf3e1eaceed2dd45
biking-flip <phips203@gmx.at>
2013-04-11 12:17:12 +0200

    JUnit-Runner für Cucumber (für tests)

---
+++ src/at/aau/reversi/test/TestRunner.java
@@ -0,0 +1,15 @@
+package at.aau.reversi.test;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phil
+ * Date: 11.04.13
+ * Time: 12:08
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(Cucumber.class)
+public class TestRunner {
+}
