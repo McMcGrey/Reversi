b4dbe18b1a7937ac307a923d112d848fdc3e214d
Arne Kohlbacher <ArneMarijan.Kohlbacher@aau.at>
2013-04-26 23:35:36 +0200

    ENUMsAITypes

---
+++ src/at/aau/reversi/enums/AIType.java
@@ -0,0 +1,35 @@
+package at.aau.reversi.enums;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arne
+ * Date: 26.04.13
+ * Time: 23:28
+ * To change this template use File | Settings | File Templates.
+ */
+public enum AIType {
+    /**
+     * Greedy AI
+     */
+    AI_GREEDY,
+    /**
+     * Random AI
+     */
+    AI_RANDOM,
+    /**
+     * Minmax AI
+     */
+    AI_MINMAX,
+    /**
+     * AlphaBetha AI
+     */
+    AI_ALPHABETHA,
+    /**
+     * Regoin AI
+     */
+    AI_REGION,
+    /**
+     * Adativ AI
+     */
+    AI_ADAPTIV;
+}
