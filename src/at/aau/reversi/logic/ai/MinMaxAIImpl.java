83f2230e57a8aa113ed964bfc7162470d7bb4b38
Arne Kohlbacher <ArneMarijan.Kohlbacher@aau.at>
2013-05-01 14:45:20 +0200

    removeIterations

--- src/at/aau/reversi/logic/ai/MinMaxAIImpl.java
+++ src/at/aau/reversi/logic/ai/MinMaxAIImpl.java
@@ -30,27 +30,23 @@ public class MinMaxAIImpl extends AbstractAIImpl implements AI {
 
         for (Move move : validMoves) {
             logic.setGameField(copyArray(gameField));
-            for (int iter = 1; iter <= iterations; iter++) {
-                logic.calcNewGameField(move.getxCoord(), move.getyCoord(), color);
-                if (!logic.possibleMoves(oponent)) {
-                    return move;
+            logic.calcNewGameField(move.getxCoord(), move.getyCoord(), color);
+            if (!logic.possibleMoves(oponent)) {
+                return move;
+            }
+            validMovesOponent = getMoves(gameField);
+            ArrayList<Integer> results = new ArrayList<Integer>();
+            int i = 100;
+            for (Move moveOponent : validMovesOponent) {
+                results.add(countFields(logic.calcNewGameField(moveOponent.getxCoord(), moveOponent.getyCoord(), oponent), color));
+            }
+            for (Integer res : results) {
+                if (res < i) {
+                    i = res;
                 }
-                validMovesOponent = getMoves(gameField);
-                ArrayList<Integer> results = new ArrayList<Integer>();
-                int i = 100;
-                for (Move moveOponent : validMovesOponent) {
-                    results.add(countFields(logic.calcNewGameField(moveOponent.getxCoord(), moveOponent.getyCoord(), oponent), color));
-                }
-                if (iter == iterations) {
-                    for (Integer res : results) {
-                        if (res < i) {
-                            i = res;
-                        }
-                    }
-                    if (i > result) {
-                        bestMove = move;
-                    }
-                }
+            }
+            if (i > result) {
+                bestMove = move;
             }
         }
         return bestMove;  //To change body of implemented methods use File | Settings | File Templates.
