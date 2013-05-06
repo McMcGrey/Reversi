65f945e54472c5721fc97bde0e1af4ddf43c6605
biking-flip <phips203@gmx.at>
2013-05-06 14:44:07 +0200

    Google json Bibliothek eingebunden

---
+++ src/at/aau/reversi/logic/Gameserver.java
@@ -0,0 +1,11 @@
+package at.aau.reversi.logic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phil
+ * Date: 06.05.13
+ * Time: 14:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class Gameserver {
+}
